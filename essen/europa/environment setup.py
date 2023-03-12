2f0434772bde1a14903d9fcade9d42cde66c40a0
cartoon72 <tdesai5091@gmail.com>
2023-03-12 20:28:12 +0530

    first commit

---
+++ essen/europa/environment setup.py
@@ -0,0 +1,5 @@
+import music21 as m21
+env = m21.environment.Environment()
+env['musescoreDirectPNGPath'] = r'C:/Program Files/MuseScore 3/bin/MuseScore3.exe'
+env['musicxmlPath'] = r'C:/Program Files/MuseScore 3/bin/MuseScore3.exe'
+
